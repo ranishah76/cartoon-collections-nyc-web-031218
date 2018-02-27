cce091421ee1b7579fb019cb4939edb42071145b
Rani Shah <rani76@gmail.com>
2018-02-27 00:35:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,13 +6,14 @@ dwarves.each.with_index(1) do |name, index|
 end
 
 def summon_captain_planet(planteer_calls)
+  planteer_calls.map { |call| call.capitalise + "!"  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planteer_calls)
+  planteer_calls.any? { |calls| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types).find 
 end
